e99897666e3d0d67e0c7dfe4759ee7218492540d
NewtonKamau <kamaunewton78@gmail.com>
2025-12-25 07:35:31 +0300

    utils

---
+++ resources/js/bootstrap.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+window.axios = axios;
+
+// Set defaults
+window.axios.defaults.withCredentials = true;
+// Set defaults
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+
+// Manually set CSRF token from meta tag if available
+const token: HTMLMetaElement | null = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
